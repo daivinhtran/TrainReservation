e432540b86cdbccb7cf397b51cc9ad8c46964be6
Vinh Tran <daivinhtran.vt@gmail.com>
2016-04-23 02:34:33 -0400

    add manager view

---
+++ manager_view.php
@@ -0,0 +1,9 @@
+<ul class="nav nav-pills nav-stacked">
+    <li><a href="view_train_schedule.php">View Train Schedule</a></li>
+    <li><a href="search_train.php">Make Reservation</a></li>
+    <li><a href="cancel_reservation.php">Cancel Reservation</a></li>
+    <li><a href="update_reservation.php">Update Reservation</a></li>
+    <li><a href="submit_review.php">Submit Review</a></li>
+    <li><a href="add_school_info.php">Add School Info</a></li>
+    <li><a href="logout.php"><strong>Log Out</strong></a></li>
+</ul>
