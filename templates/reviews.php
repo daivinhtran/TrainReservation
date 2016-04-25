27a6a7d2541ef2f5b456e1fbc807195c1af39409
Vinh Tran <daivinhtran.vt@gmail.com>
2016-04-24 23:11:01 -0400

    add view revenue and popular route

--- templates/reviews.php
+++ templates/reviews.php
@@ -1,17 +1,3 @@
-<!-- <form action="view_review.php" class="form-horizontal" method="post">
-    <fieldset>
-        <div class="form-group">
-            <select class="form-control" id="trainNum" name = "trainNum" required>
-            <?php foreach($train_numbers as $train_number): ?>
-              <option><?= $train_number["train_number"] ?></option>
-            <?php endforeach ?>
-            </select>
-        </div>
-        <div class="form-group">
-            <button type="submit" class="btn btn-primary">Next</button>
-        </div>
-    </fieldset>
-</form> -->
 <ul class="nav nav-pills nav-justified">
     <li><a href="view_train_schedule.php">View Train Schedule</a></li>
     <li><a href="search_train.php">Make Reservation</a></li>
