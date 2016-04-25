1cbcaa7744d91e0f3c1f77db4422be60f11e45f6
Vinh Tran <daivinhtran.vt@gmail.com>
2016-04-24 22:38:23 -0400

    almost final

--- includes/functions.php
+++ includes/functions.php
@@ -232,4 +232,14 @@
         }
     }
 
+    function endsWith($haystack, $needle)
+    {
+        $length = strlen($needle);
+        if ($length == 0) {
+            return true;
+        }
+
+        return (substr($haystack, -$length) === $needle);
+    }
+
 ?>
--- templates/customer_view.php
+++ templates/customer_view.php
@@ -12,7 +12,8 @@
     <li><a href="search_train.php">Make Reservation</a></li>
     <li><a href="cancel_reservation.php">Cancel Reservation</a></li>
     <li><a href="update_reservation.php">Update Reservation</a></li>
+    <li><a href="view_review.php">View Review</a></li>
     <li><a href="review.php">Submit Review</a></li>
-    <li><a href="add_school_info.php">Add School Info</a></li>
+    <li><a href="verify_student.php">Verify Student Status</a></li>
     <li><a href="logout.php"><strong>Log Out</strong></a></li>
 </ul>
