1a9116e1a6f3da0d8d2ce33c8262ebedfcd36a1c
Vinh Tran <daivinhtran.vt@gmail.com>
2016-04-24 23:38:36 -0400

    connect with online server DB

--- includes/constants.php
+++ includes/constants.php
@@ -9,28 +9,28 @@
      * Global constants.
      */
 
-    // your database's name
-    // define("DATABASE", "cs4400_Team_30");
-    //
-    // // your database's password
-    // define("PASSWORD", "mySZsA8w");
-    //
-    // // your database's server
-    // define("SERVER", "academic-mysql.cc.gatech.edu");
-    //
-    // // your database's username
-    // define("USERNAME", "cs4400_Team_30");
-
-    // your database's name
-    define("DATABASE", "TrainSystem_v02");
+    //your database's name
+    define("DATABASE", "cs4400_Team_30");
 
     // your database's password
-    define("PASSWORD", "root");
+    define("PASSWORD", "mySZsA8w");
 
     // your database's server
-    define("SERVER", "localhost:8889");
+    define("SERVER", "academic-mysql.cc.gatech.edu");
 
     // your database's username
-    define("USERNAME", "root");
+    define("USERNAME", "cs4400_Team_30");
+
+    // // your database's name
+    // define("DATABASE", "TrainSystem_v02");
+    //
+    // // your database's password
+    // define("PASSWORD", "root");
+    //
+    // // your database's server
+    // define("SERVER", "localhost:8889");
+    //
+    // // your database's username
+    // define("USERNAME", "root");
 
 ?>
